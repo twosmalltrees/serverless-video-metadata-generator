bed60a789e1eb92642a943b2e2e03b215ded48b5
Harrison Reid <harrison@twosmalltrees.com>
2017-12-21 23:38:56 +1100

    Test

--- handler.js
+++ handler.js
@@ -35,6 +35,7 @@ module.exports.extractMetadata = (event, context, callback) => {
 };
 
 module.exports.saveMetadata = (event, context, callback) => {
+  console.log(event.Records[0].Sns);
   const jobId = event.Records[0].Sns.JobId;
   const bucketName = event.Records[0].Sns.Video.S3Bucket;  
   const objectKey = event.Records[0].Sns.Video.S3ObjectName;
