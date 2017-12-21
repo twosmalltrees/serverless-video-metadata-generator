e4da725192fa05b43173789c4d5d25bf238533a3
Harrison Reid <harrison@twosmalltrees.com>
2017-12-21 20:46:49 +1100

    Implement extractMetadata lambda

--- handler.js
+++ handler.js
@@ -3,16 +3,17 @@ import AWS from 'aws-sdk';
 
 
 module.exports.extractMetadata = (event, context, callback) => {
-  
+  const bucketName = event.Records[0].s3.bucket.name;
+  const objectKey = event.Records[0].s3.object.key;
   const params = {
     Video: {
       S3Object: {
-        Bucket: 'hopeully-passed-in-event',
-        Name: 'hopefully-passed-in-event',
+        Bucket: bucketName,
+        Name: objectKey,
       },
       NotificationChannel: {
-        RoleArn: 'unsusre',
-        SNSTopicArn: 'topicARN'
+        RoleArn: process.env.SNS_PUBLISH_ROLE_ARN,
+        SNSTopicArn: process.env.SNS_TOPIC_ARN,
       }
     }
   }
@@ -20,15 +21,12 @@ module.exports.extractMetadata = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Successfully processed',
       input: event,
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 module.exports.saveMetadata = (event, context, callback) => {
